25159031e28f99df9d723f0bee3d52f5af4f48cd
dmwindyhighway <59108030+dmwindyhighway@users.noreply.github.com>
2019-12-23 16:35:15 -0800

    Update UpdateSalesInvoice.php

--- UpdateSalesInvoice.php
+++ UpdateSalesInvoice.php
@@ -3,7 +3,8 @@ ini_set('display_errors','1');
 ini_set('display_startup_errors','1');
 error_reporting (E_ALL); 
 
-
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+$coverage = new CodeCoverage;
       
 require_once "PostXml.php";
 require_once "PostXml2.php";
